0f293961ccfcb71a30497ffc3b2589ce6b45e3fe
Sindre Sorhus <sindresorhus@gmail.com>
2018-04-07 19:27:13 +0700

    Init

---
+++ test.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+import execa from 'execa';
+import timeSpan from 'time-span';
+
+test('main', async t => {
+	const end = timeSpan();
+	await execa('./cli.js', [2]);
+	t.true(end.seconds() >= 2);
+});
