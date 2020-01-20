dfa53735bd6f670b978e5564c6875e6650158ad8
Jason Lee <jason@zerodevx.com>
2020-01-21 01:51:19 +0800

    Prep for v1

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ import timeSpan from 'time-span';
 
 test('main', async t => {
   const end = timeSpan();
-  await execa('./cli.js', [2]);
-  t.true(end.seconds() >= 2);
+  await execa('./cli.js', [2.5]);
+  const result = end.seconds();
+  t.true(result > 2);
+  t.true(result < 3);
 });
